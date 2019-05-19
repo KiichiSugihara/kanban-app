13e280bf7b4a0265bb844c4c579016b76d3a8ab7
Kiichi Sugihara <kiichi.sugihara@gmail.com>
2019-05-19 15:35:19 +0900

    Build BacendApiServer

---
+++ build/dev-server.js
@@ -0,0 +1,9 @@
+// Node.jsのrequireスタイルでインポート
+const bodyParser = require('body-parser');
+
+// `Express`アプリケーションインスタンスを受取る関数をエクスポート
+module.exports = app => {
+  // HTTPリクエストのbodyの内容をJSONとして解析するようミドルウェアをインストール
+  app.use(bodyParser.json());
+  // TODO APIの内容実装
+};
